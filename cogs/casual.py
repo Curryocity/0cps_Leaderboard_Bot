59e85b0bf1bdc51fc9f9b230322a8649ae0ab658
Curryocity <curryocity87@gmail.com>
2024-09-07 21:49:30 +0800

    69

--- cogs/casual.py
+++ cogs/casual.py
@@ -15,7 +15,7 @@ class ButtonView(View):
             self.button.press_count += 1
             self.button.label = f"Pressed {self.button.press_count} times"
             await interaction.response.edit_message(content=f"Button pressed {self.button.press_count} times", view=self)
-            if self.button.press_count == 5:
+            if self.button.press_count == 69:
                 await interaction.followup.send("Noice~")
             
 
